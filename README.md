2d33542da47209c2e26d5ab6d4628fefb644105a
Katlyn Lorimer <katlyn@katlyn.dev>
2025-02-25 15:53:18 -0900

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# video-platform
+## Development
+```bash
+# To start the dev server
+npm run dev
+
+# To start the production server
+npm start
+```
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import * as path from "node:path"
+
+const app = express()
+app.use(express.static(path.join(import.meta.dirname, "static")))
+
+const listener = app.listen(3000, () => {
+    const { port } = listener.address()
+    console.log(`Listening on http://localhost:${port}/`)
+})
