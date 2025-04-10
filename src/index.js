fd5296422244ed8cc9faaf00db302995a8d9b787
Katlyn Lorimer <katlyn@katlyn.dev>
2025-04-09 22:23:16 -0800

    fix: minor formatting mistake

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import session from "express-session";
 
 const app = express();
 app.use(session({
-    maxAge: 24 * 60 * 60 * 1000,// 24 hours
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
     resave: true,
     saveUninitialized: true,
     // TODO: we need a better secret i guess.
