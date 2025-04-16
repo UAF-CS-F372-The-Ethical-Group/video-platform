00583be72b042844f37e09baaf0c8c36c86d4eef
Katlyn Lorimer <katlyn@katlyn.dev>
2025-04-15 17:07:45 -0800

    fix: add mime type to db

--- src/mongodb.ts
+++ src/mongodb.ts
@@ -4,9 +4,10 @@
  */
 
 import { MongoClient } from "mongodb";
+import { Like, Movie, User } from "./types.ts";
 
 export const client = new MongoClient("mongodb://localhost:27017");
 export const db = client.db("video-platform");
-export const userCollection = db.collection("users");
-export const movieCollection = db.collection("movies");
-export const likeCollection = db.collection("likes");
+export const userCollection = db.collection<User>("users");
+export const movieCollection = db.collection<Movie>("movies");
+export const likeCollection = db.collection<Like>("likes");
--- src/scripts/seedDatabase.ts
+++ src/scripts/seedDatabase.ts
@@ -1,9 +1,10 @@
 /**
  * Adds sample data to the database to ease the development process.
  */
-
+import { Collection } from "mongodb";
 import hashPassword from "../hashPassword.ts";
 import { client, db, likeCollection, movieCollection } from "../mongodb.ts";
+import { Like, Movie } from "../types.ts";
 
 await db.dropCollection("users");
 await db.dropCollection("movies");
@@ -16,26 +17,31 @@ const createdUser = await userCollection.insertOne({
   role: "viewer",
 });
 
-const createdMovieResponse = await movieCollection.insertMany([
-  {
-    title: "Ducky",
-    genre: "Birds",
-    videoPath: "/data/265501_tiny.mp4",
-    thumbnailPath: "/data/265501_tiny_thumb.png",
-  },
-  {
-    title: "Waves",
-    genre: "Water",
-    videoPath: "/data/244839_tiny.mp4",
-    thumbnailPath: "/data/244839_tiny_thumb.png",
-  },
-  {
-    title: "Waterfall",
-    genre: "Water",
-    videoPath: "/data/246856_tiny.mp4",
-    thumbnailPath: "/data/246856_tiny_thumb.png",
-  },
-]);
+const createdMovieResponse =
+  await (movieCollection as unknown as Collection<Omit<Movie, "_id">>)
+    .insertMany([
+      {
+        title: "Ducky",
+        genre: "Birds",
+        videoPath: "/data/265501_tiny.mp4",
+        videoMimeType: "video/mp4",
+        thumbnailPath: "/data/265501_tiny_thumb.png",
+      },
+      {
+        title: "Waves",
+        genre: "Water",
+        videoPath: "/data/244839_tiny.mp4",
+        videoMimeType: "video/mp4",
+        thumbnailPath: "/data/244839_tiny_thumb.png",
+      },
+      {
+        title: "Waterfall",
+        genre: "Water",
+        videoPath: "/data/246856_tiny.mp4",
+        videoMimeType: "video/mp4",
+        thumbnailPath: "/data/246856_tiny_thumb.png",
+      },
+    ]);
 
 const createdMovieIds = Object.values(createdMovieResponse.insertedIds);
 // Take half of the created movies, like some, and dislike others
@@ -48,6 +54,8 @@ const likes = createdMovieIds.slice(0, createdMovieIds.length / 2 + 1).map((
   status: idx % 2 === 0,
 }));
 
-await likeCollection.insertMany(likes);
+await (likeCollection as unknown as Collection<Omit<Like, "_id">>).insertMany(
+  likes,
+);
 
 await client.close();
